726311f6878c0495116bbb1117f5ed69eed7b7c9
Gabewest1 <gabe_west1@yahoo.com>
2017-08-15 19:28:30 -0500

    Created a file to get the alarm and tick audio

---
+++ scripts/AlarmAudio.js
@@ -0,0 +1,8 @@
+let AlarmAudio
+
+if (!AlarmAudio) {
+    console.log("I WANT TO SEE MYSELF ONLY ONCE")
+    AlarmAudio = new Audio("../assets/electronic_beeping_alarm_clock.mp3")
+}
+
+export default AlarmAudio
---
+++ scripts/TickAudio.js
@@ -0,0 +1,8 @@
+let TickAudio
+
+if (!TickAudio) {
+    console.log("AYYYYY ONLY ONCE")
+    TickAudio = new Audio("../assets/electronic_alarm_clock_beep.mp3")
+}
+
+export default TickAudio
