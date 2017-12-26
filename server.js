df4d097f259a19d079142e4362566d113b60d298
Gabriel West <gabe_west1@yahoo.com>
2017-12-26 01:56:58 -0600

    Updated server to use express static middleware and listen for the bundle.js request.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express")
+const path = require("path")
 const PORT = process.env.PORT || 3000
 const app = express()
 
+app.use(express.static(path.resolve(__dirname, "assets")))
+app.use("/bundle.js", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "bundle.js"))
+})
 app.use("/", (req, res) =>{
     res.sendFile(__dirname + "/index.html")
 })
